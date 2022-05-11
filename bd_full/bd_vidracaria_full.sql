af0f09e30d53fb4fd437a63c94b13a6e5c03096c
Gabriel Cardoso <gabrieldominguesitap@gmail.com>
2022-05-11 15:25:20 -0300

    viw

--- bd_full/bd_vidracaria_full.sql
+++ bd_full/bd_vidracaria_full.sql
@@ -240,30 +240,4 @@ ALTER TABLE `tb_produtos`
 	ADD CONSTRAINT `id_categoria_produto_fk` FOREIGN KEY (`id_categoria_produto`)
     REFERENCES `tb_categoria` (`id_categoria`)
 	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
-        
--- Criando a view lista de produto--------------------------------------------------
-CREATE VIEW `vw_tbprodutos` AS
-	SELECT	p.codigo_produto,
-			p.descri_produto,
-            cor.rotulo_cor,
-            e.rotulo_espessura,
-            u.sigla_unidade,
-            c.rotulo_categoria,
-            p.fabricante_produto,
-            p.custo_produto,
-            p.venda_produto,
-            p.fornecedor_produto,
-            p.imagem_produto,
-            p.destaque_produto
-	FROM 	tb_produtos p 
-    INNER join tb_cor cor ON p.id_cor_produto = cor.id_cor
-    INNER JOIN tb_categoria c ON P.id_categoria_produto = c.id_categoria
-    INNER join tb_unidade u ON p.id_unidade_produto = u.id_unidade
-    INNER join tb_espessura e ON p.id_unidade_produto = e.id_espessura; 
-    
-    
- 
--- chamando a view-------------------------
-      SELECT *
-       FROM  vw_tbprodutos       
+	ON UPDATE NO ACTION;  
