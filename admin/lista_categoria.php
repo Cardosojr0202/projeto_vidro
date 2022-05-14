37ab87c7d772e4975d82cd456e21ec5cd044d485
Gabriel Cardoso <gabrieldominguesitap@gmail.com>
2022-05-14 14:44:17 -0300

    lista
    lista categoria quase pronta

--- admin/lista_categoria.php
+++ admin/lista_categoria.php
@@ -19,29 +19,45 @@ $totalrows = ($list)->fetch_assoc();
     <link rel="stylesheet" href="../css/bootstrap.min.css" >
     <!-- Link para CSS específico -->
     <link rel="stylesheet" href="../css/meu_estilo.css">
+    <!-- Link para icones do bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
 </head>
-<body>
+<body class="fundofixo">
     <main class="container"><br>
-        <h1 class="bg-primary bg-gradient">lista de categoria</h1>
+        <h1 class="bg-primary bg-gradient">lista de Categoria</h1>
         <div>
-            <table class="table table-primary table-hover tbopacidade">
+            <table class="table table-hover tbopacidade">
              <!-- thead>tr>th*8 -->
              <thead>
                  <tr>
-                     <th class="col">ID</th>
-                     <th class="col">SIGLA</th>
-                     <th class="col">ROTULO</th>
-                     <th class="col">ADICIONAR</th>
+                     <th class="d-none">ID</th>
+                     <th>SIGLA</th>
+                     <th>ROTULO</th>
+                     <th>
+                         <a href="#" target="_self" class="btn btn-primary d-block" role="button">
+                            <i class="d-xs-none">ADICIONAR </i>
+                            <i class="bi bi-plus-circle"></i>
+                         </a>
+                    </th>
                  </tr>
              </thead>
               <!-- tbody>tr>td*8 -->
               <tbody>
                   <?php do { ?>
                   <tr>
-                        <td><?php echo $row['id_categoria'] ?></td>
+                        <td class="d-none"><?php echo $row['id_categoria'] ?></td>
                         <td><?php echo $row['sigla_categoria'] ?></td>
                         <td><?php echo $row['rotulo_categoria'] ?></td>
-                        <td>ALTERAR|EXCLUIR</td>
+                        <td class="d-grid gap-2">
+                            <a href="#" class="btn btn-success" role="button">
+                                <i>ALTERAR</i>
+                                <i class="bi bi-arrow-clockwise"></i>
+                            </a>
+                            <button data-nome="" data-id="" class="btn btn-danger">
+                                <i>EXCLUIR</i>
+                                <i class="bi bi-trash-fill"></i>
+                            </button>
+                        </td>
                   </tr>
                   <?php } while ($row = $list->fetch_assoc()); ?>
               </tbody>
