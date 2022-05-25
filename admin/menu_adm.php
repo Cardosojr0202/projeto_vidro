db2d85e2e57644b356fe28c5cb80696e30a94fc1
Gabriel cardoso <gabrieldominguesitap@gmail.com>
2022-05-25 15:44:36 -0300

    menu

--- admin/menu_adm.php
+++ admin/menu_adm.php
@@ -4,56 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Área Administrativa</title>
-    <!-- CÓDIGO DESABILITADO PARA NÃO HAVER CONFLITOS
-    <link rel="stylesheet" href="../css/bootstrap.min.css" rel="stylesheet">
-    -->
-    <!-- Link para CSS específico -->
+    <title>Menu | Front</title>
+    <!-- CSS do menu -->
+    <link rel="stylesheet" href="../css/menu.css">
+    <!-- CSS Bootstrap -->
+    <!-- <link rel="stylesheet" href="../css/bootstrap.min.css"> -->
+    <!-- Icones do bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
 </head>
 <body>
-<nav class="nav navbar-inverse">
-<div class="container-fluid">
-    <!-- agrupamento para exibição MOBILE -->
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#defaultNavbar" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a href="#" class="navbar-brand">
-            <img src="../imagens/logochurrascopequeno.png" alt="">
-        </a>
-    </div> <!-- fecha navbar-header -->
-    <!-- fecha agrupamento para exibição MOBILE -->
-
-    <!-- nav direita -->
-    <div class="collapse navbar-collapse" id="defaultNavbar">
-        <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="index.php">ADMIN</a></li>
-            <li><a href="produto_lista.php">Produtos</a></li>
-            <li><a href="">Categoria</a></li>
-            <li><a href="">Colaborador</a></li>
-            <li class="active">
-                <a href="">
-                    <span class="glyphicon glyphicon-home"></span>
-                </a>
-            </li>
-            <li>
-                <a href="logout.php">
-                    <span class="glyphicon glyphicon-log-out"></span>
-                </a>
-            </li>
-        </ul>
-    </div> <!-- fecha navbar-collapse -->
-    <!-- fecha nav direita -->
-</div> <!-- fecha container-fluid -->
-</nav>
-
-<!-- Link arquivos bootstrap script js -->
-<!-- CÓDIGO DESABILITADO PARA NÃO HAVER CONFLITOS
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>
--->
+    <input type="checkbox" id="check">
+    <!--header começo-->
+    <header>
+        <label for="check">
+            <ion-icon name="menu-outline" id="sidebar_btn"></ion-icon>
+        </label>
+        <div class="logo">
+            <h3>Front <span>Vidraçaria</span></h3>
+        </div>
+        <div class="btn-right">
+            <a href="#" class="sair_btn">Sair</a>
+        </div>
+    </header>
+    <!--header final-->
+    <!--sidebar começo-->
+    <div class="sidebar">
+        <h2 class="text-center">Olá Maria<br>Seja bem vinda</h2>
+        <a href="#"><ion-icon name="desktop-outline"></ion-icon><span>Painel</span></a>
+        <a href="produto_lista.php"><ion-icon name="calendar-clear-outline"></ion-icon><span>Prodrutos</span></a>
+        <a href="categoria_lista.php"><ion-icon name="camera-outline"></ion-icon><span>Categoria</span></a>
+        <a href="colaborador_lista.php"><ion-icon name="information-circle-outline"></ion-icon><span>Colaboradores</span></a>
+        <a href="#"><ion-icon name="settings-outline"></ion-icon><span>Configuração</span></a>
+    </div>
+    <!--sidebar final-->
+    <div class="content"></div>
+    
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <!-- Link arquivos bootstrap script js -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
