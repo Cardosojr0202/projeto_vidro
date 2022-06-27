e4e3049551676b51d23826aef2b1cac12110cf57
Gabriel cardoso <90519762+Cardosojr0202@users.noreply.github.com>
2022-06-27 12:39:00 -0300

    card-dev-feito

--- developer.php
+++ developer.php
@@ -18,7 +18,7 @@
       <div class="row">
          <div class="container d-flex justify-content-center  flex-wrap" style="margin-top: 10rem; margin-bottom: 6rem;">
          
-            <div class="card-dev">
+            <div class="card-dev" style="margin: 36px;">
                <div class="imgBg">
                   <img src="imagens/perfil/perfil-2.jpg" alt="">
                </div>
@@ -26,9 +26,28 @@
                   <div class="details">
                      <h2>Gabriel cardoso<br><span>junior ux/ui Designer</span></h2>
                      <div class="data">
-                        <h3>322<br><span>Posts</span></h3>
-                        <h3>120k<br><span>Followers</span></h3>
-                        <h3>280<br><span>Following</span></h3>
+                        <ul class="nav text-center d-flex justify-content-center mx-auto">
+                           <li class="nav-item mx-2">
+                               <a href="#home" class="text-info">
+                                   <i class="bi bi-facebook" style="font-size: 3rem;"></i>
+                               </a>
+                           </li>
+                           <li class="nav-item mx-2">
+                               <a href="#home" class="text-danger">
+                                   <i class="bi bi-instagram" style="font-size: 3rem;"></i>
+                               </a>
+                           </li>
+                           <li class="nav-item mx-2">
+                               <a href="#home" class="text-info">
+                                   <i class="bi bi-twitter" style="font-size: 3rem;"></i>
+                               </a>
+                           </li>
+                           <li class="nav-item mx-2">
+                               <a href="#home" class="text-success" style="font-size: 3rem;">
+                                   <i class="bi bi-whatsapp"></i>
+                               </a>
+                           </li>
+                       </ul>
                      </div> 
                      <div class="actionBtn">
                         <button class="btn">Follow</button>
@@ -39,7 +58,44 @@
             </div><!-- Fecha card-dev -->
 
 
-            
+            <div class="card-dev" style="margin: 36px;">
+               <div class="imgBg">
+                  <img src="imagens/perfil/perfil-1.jpeg" alt="">
+               </div>
+               <div class="content">
+                  <div class="details">
+                     <h2>Claudio milton<br><span>junior ux/ui Designer</span></h2>
+                     <div class="data">
+                        <ul class="nav text-center d-flex justify-content-center mx-auto">
+                           <li class="nav-item mx-2">
+                               <a href="#home" class="text-info">
+                                   <i class="bi bi-facebook" style="font-size: 3rem;"></i>
+                               </a>
+                           </li>
+                           <li class="nav-item mx-2">
+                               <a href="#home" class="text-danger">
+                                   <i class="bi bi-instagram" style="font-size: 3rem;"></i>
+                               </a>
+                           </li>
+                           <li class="nav-item mx-2">
+                               <a href="#home" class="text-info">
+                                   <i class="bi bi-twitter" style="font-size: 3rem;"></i>
+                               </a>
+                           </li>
+                           <li class="nav-item mx-2">
+                               <a href="#home" class="text-success" style="font-size: 3rem;">
+                                   <i class="bi bi-whatsapp"></i>
+                               </a>
+                           </li>
+                       </ul>
+                     </div> 
+                     <div class="actionBtn">
+                        <button class="btn">Follow</button>
+                        <button class="btn">Menssage</button>
+                     </div>
+                  </div>
+               </div>
+            </div><!-- Fecha card-dev -->
 
          </div>
       </div><!-- Fecha Row -->
